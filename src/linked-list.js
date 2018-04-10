c19a98d4f291a268afd2f5f21d2bf7c913bcb3eb
dmoralestech <darwin.morales.tech@gmail.com>
2018-04-11 09:32:50 +1000

    initial Implementation

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,1 +1,20 @@
 
+function LinkedList() {
+  var Node = (value) => {
+    this.value = value;
+    this.next = null;
+  };
+  
+  var length = 0;
+  var head = null;
+  
+  this.append = (value) => {};
+  this.insert = (value, index) => {};
+  this.removeAt = (index) => {};
+  this.remove = (value) => {};
+  this.indexOf = (value) => {};
+  this.isEmpty = () => {};
+  this.size = () => {};
+  this.toString = () => {};
+  this.print= () => {};
+}
