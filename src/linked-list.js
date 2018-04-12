1ae0188f491908b3fddfb76451b81e8ffbe1d545
dmoralestech <darwin.morales.tech@gmail.com>
2018-04-12 21:38:00 +1000

    Add append function

--- src/linked-list.js
+++ src/linked-list.js
@@ -8,7 +8,19 @@ function LinkedList() {
   var length = 0;
   var head = null;
   
-  this.append = (value) => {};
+  this.append = (value) => {
+    var node = new Node(value);
+    if (head === null) {
+      head = node;
+    } else {
+      var current = head;
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = node;
+    }
+    length++;
+  };
   this.insert = (value, index) => {};
   this.removeAt = (index) => {};
   this.remove = (value) => {};
