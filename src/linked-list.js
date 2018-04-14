cfd2a5c0c2606e58808dfb36e3791e0b51e7cddc
darwinmorales <darwin.morales.tech@gmail.com>
2018-04-15 08:07:00 +1000

    Just use console.assert

--- src/linked-list.js
+++ src/linked-list.js
@@ -116,11 +116,11 @@ function LinkedList() {
   }
 }
 
-function assert(condition, message) {
-  if (!condition) {
-    throw message || "Assertion failed";
-  }
-}
+// function assert(condition, message) {
+//   if (!condition) {
+//     throw message || "Assertion failed";
+//   }
+// }
 
 var linkedList = new LinkedList();
 
@@ -140,7 +140,7 @@ linkedList.append("g");
 linkedList.remove("a");
 linkedList.remove("c");
 console.log("size", linkedList.size());
-assert(linkedList.size() === 5);
+console.assert(linkedList.size() === 6);
 linkedList.print();
 
 // console.log(linkedList.indexOf("b"));
