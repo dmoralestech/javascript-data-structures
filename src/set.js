7b9e776aa1240caa5b4f0c5b3826aef4b78148be
dmoralestech <darwin.morales.tech@gmail.com>
2018-05-06 11:57:37 +1000

    Update set.js

--- src/set.js
+++ src/set.js
@@ -3,6 +3,14 @@ function Set() {
   var items =  {};
   
   this.has = (value) => {
-    return value in items;
+    return items.hasOwnProperty(value);
+  }
+  
+  this.append = (value) => {
+    if (this.has(value)) {
+      return false;
+    }
+    items[value] = value;
+    return true;
   }
 }
