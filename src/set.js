a0b2ce146a10695808ac47341a3cc68b4e13b7cb
darwinmorales <darwin.morales.tech@gmail.com>
2018-05-06 12:17:33 +1000

    refactor

--- src/set.js
+++ src/set.js
@@ -1,47 +1,49 @@
-
 function Set() {
-  let items =  {};
-  
-  this.has = (value) => {
+  let items = {};
+
+  this.has = value => {
     return items.hasOwnProperty(value);
-  }
-  
-  this.append = (value) => {
+  };
+
+  this.append = value => {
     if (this.has(value)) {
       return false;
     }
     items[value] = value;
     return true;
-  }
-  
-  this.remove = (value) => {
+  };
+
+  this.remove = value => {
     if (!this.has(value)) {
       return false;
-    } 
-    
+    }
+
     delete items[value];
     return true;
-  }
-  
+  };
+
   this.clear = () => {
     items = {};
-  }
-  
+  };
+
   this.size = () => {
     return Object.keys(items).length;
-  }
-  
+  };
+
   this.values = () => {
     return Object.keys(items);
-  }
-  
-  this.union = (anotherSet) => {
+  };
+
+  this.union = anotherSet => {
     let resultSet = new Set();
-    
-    Object.keys(items).forEach( item => { resultSet.add(item); });
-    Object.keys(anotherSet).forEach( item => { resultSet.add(item); });
-    
+
+    Object.keys(items).forEach(item => {
+      resultSet.add(item);
+    });
+    Object.keys(anotherSet).forEach(item => {
+      resultSet.add(item);
+    });
+
     return resultSet;
-    
-  }
+  };
 }
