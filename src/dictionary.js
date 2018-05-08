613964b7914bb01df89c66f39be10aea4017a5d6
darwinmorales <darwin.morales.tech@gmail.com>
2018-05-08 20:38:43 +1000

    dictionary()

--- src/dictionary.js
+++ src/dictionary.js
@@ -1,4 +1,22 @@
 function Dictionary() {
   "use strict";
+  let items = {};
+
+  this.has = value => {
+    return items.hasOwnProperty(value);
+  };
+
+  this.set = (key, value) => {
+    items[key] = valiue;
+  };
+
+  this.remove = (key) => {
+    if (this.has(key)) {
+      delete items[key];
+      return true;
+    }
+    return false;
+  }
+
 
 }
